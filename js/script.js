dbbe3f9d6e1d582b68f7bd2cd7a7d8fcbfbc7928
clevercleaver1 <nightandday4@gmAil.com>
2016-11-26 13:35:16 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  var buildAndShowHomeHTML = {}, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  var obj = JSON.parse(buildAndShowHomeHTML);, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
